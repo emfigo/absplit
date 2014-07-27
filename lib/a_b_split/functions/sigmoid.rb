d61705f03fd0ad02e9573a8149722aa8c158997e
Enrique Figuerola <emfigo@gmail.com>
2014-07-27 20:16:44 +0100

    Add sigmoid function

--- lib/a_b_split/functions/sigmoid.rb
+++ lib/a_b_split/functions/sigmoid.rb
@@ -1,6 +1,11 @@
+require 'bigdecimal'
+
 module ABSplit
   module Functions
     class Sigmoid
+      def self.value_for(x)
+        BigDecimal.new("#{1.0/(1 + Math.exp(-2 * x))}")
+      end
     end
   end
 end
