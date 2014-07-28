92660e9391d9867a299a2f1f3a1633b033fb84ed
Enrique Figuerola <emfigo@gmail.com>
2014-07-28 23:33:54 +0100

    Use default weighted function for ab split

--- lib/a_b_split/test.rb
+++ lib/a_b_split/test.rb
@@ -1,11 +1,11 @@
-require 'cgi'
-
 module ABSplit
   module Test
-    def split(name, *args)
-      cgi = CGI.new
+    def self.split(name, x)
+      experiment = ABSplit.configuration.experiments[name]
 
+      raise ABSplit::NoValidExperiment unless experiment
 
+      ABSplit::Functions::WeightedSplit.value_for(x,*experiment)
     end
   end
 end
--- spec/a_b_split/test_spec.rb
+++ spec/a_b_split/test_spec.rb
@@ -1,10 +1,34 @@
 require 'spec_helper'
 
 describe ABSplit::Test do
-  include ABSplit::Test
+  let(:x) { 'testa123' }
+  let(:experiment) { { experiment:
+                          [ { name: 'optiona', weight: 50 },
+                            { name: 'optionb'} ]
+                    } }
+  describe '.split' do
+    context 'when no experiment file is specified' do
+      before do
+        ABSplit.configure
+      end
+      
+      it 'splits the experiment with the default function' do
+        expect{ described_class.split(:experiment, x) }.to raise_error(ABSplit::NoValidExperiment)
+      end
+    end
 
-  describe '#split' do
-    it '' do
+    context 'when an experiment file is specified' do
+      before do
+        ABSplit.configure do |config|
+          config.experiments = experiment 
+        end
+      end
+
+      it 'splits the experiment with the default function' do
+        expect(ABSplit::Functions::WeightedSplit).to receive(:value_for).with(x, *experiment[:experiment])
+
+        described_class.split(:experiment, x)
+      end
     end
   end
 end
