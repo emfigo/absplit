2c4524901dcacf5910e41af1fd98e1d8958c6ac2
Enrique Figuerola <emfigo@gmail.com>
2014-07-28 23:32:00 +0100

    Unify functions with same call

--- lib/a_b_split/functions/sigmoid.rb
+++ lib/a_b_split/functions/sigmoid.rb
@@ -3,7 +3,7 @@ require 'bigdecimal'
 module ABSplit
   module Functions
     class Sigmoid
-      def self.value_for(x)
+      def self.value_for(x, *params)
         BigDecimal.new("#{1.0/(1 + Math.exp(-2 * x))}")
       end
     end
